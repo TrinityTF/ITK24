3164ffb78a0fb250f05c94fe96b12b2e5321c986
Agu Toomas Pihelgas <68427806+TrinityTF@users.noreply.github.com>
2024-11-27 12:19:19 +0200

    Add files via upload

--- arvaAra/Model.py
+++ arvaAra/Model.py
@@ -2,24 +2,33 @@ import random
 
 
 class Model:
+
+    # Klassi muutujad
+    pc_nr = 0
+    steps = 0
+    gameOver = False
+
     def __init__(self):
+        self.gameReset()
+        self.filename = "result.txt"
+
+    def gameReset(self):
         self.pc_nr = random.randint(1, 100)
         self.steps = 0
         self.gameOver = False
-        self.filename = "result.txt"
 
     def ask(self):
-        userNum = int(input("Sisesta number: "))
+        user = int(input("Sisesta number: "))
         self.steps += 1 # +1
 
-        if userNum == 1000:
+        if user == 1000:
             print(f"Leidsid nõrga koha. Õige number on {self.pc_nr}.")
             self.gameOver = True
-        elif userNum > self.pc_nr:
+        elif user > self.pc_nr:
             print("Väiksem")
-        elif userNum < self.pc_nr:
+        elif user < self.pc_nr:
             print("Suurem")
-        elif userNum == self.pc_nr:
+        elif user == self.pc_nr:
             print(f"Leidisd õige arvu {self.steps} käiguga.")
             self.gameOver = True
 
@@ -29,6 +38,10 @@ class Model:
 
         name = self.askName()
         self.addNameToFile(name)
+
+        if self.playAgain():
+            self.gameReset()
+            self.letsPlay()
 
     def askName(self):
         name = input("Sisesta enda nimi: ")
@@ -40,4 +53,11 @@ class Model:
         with open(self.filename, "a", encoding="utf-8") as file:
             line = f"{name};{self.pc_nr};{self.steps}\n"
             file.write(line)
+
+    def playAgain(self):
+        user = input("Kas mängid uuesti? [ Jah / Ei ]")
+        if user.lower() == "jah" or user.lower() == "j":
+            return True
+        return False
+
 
