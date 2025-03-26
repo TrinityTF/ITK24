bed2bd17fc05dfe580daa6cffd90608dc580d73a
TrinityTF <agutoomasp@gmail.com>
2025-03-26 09:59:58 +0200

    tunni muudatused

--- Skriptid/read-person-v1.ps1
+++ Skriptid/read-person-v1.ps1
@@ -10,6 +10,19 @@ $dst = Join-Path -Path $PSScriptRoot -ChildPath 'Persons-v1.csv' # Uus fail
 $domeen = "@singedhaircut.gg" # Domeen
 $header = "Eesnimi;Perenimi;Sünniaeg;Kasutajanimi;E-Mail" # Uus faili päis
 
+# Rõhumärkide eemaldamise funktsioon
+function Remove-Diacritics {
+    param ([String]$src = [String]::Empty)
+    $normalized = $src.Normalize( [Text.NormalizationForm]::FormD )
+    $sb = new-object Text.StringBuilder
+    $normalized.ToCharArray() | ForeEach-Object { 
+        if ( [Globalization.CharUnicodeInfo]::GetUnicodeCategory($_) -ne [Globalization.UnicodeCategory]::NonSpacingMark) {
+            [void]$sb.Append($_)
+        }
+    }
+    $sb.ToString()
+}
+
 # Kontrollime, kas uus fail on olemas, kui on, siis eemalda
 if (Test-Path $dst) {
     Remove-Item -Path $dst
@@ -47,17 +60,4 @@ foreach ($line in $contents){
 
     # Lisame rea uude faili
     Out-File -FilePath $dst -Append -InputObject $newLine
-}
-
-# Rõhumärkide eemaldamise funktsioon
-function Remove-Diacritics {
-    param ([String]$src = [String]::Empty)
-    $normalized = $src.Normalize( [Text.NormalizationForm]::FormD )
-    $sb = new-object Text.StringBuilder
-    $normalized.ToCharArray() | ForeEach-Object { 
-        if ( [Globalization.CharUnicodeInfo]::GetUnicodeCategory($_) -ne [Globalization.UnicodeCategory]::NonSpacingMark) {
-            [void]$sb.Append($_)
-        }
-    }
-    $sb.ToString()
 }
